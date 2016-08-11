677403abaacae022af1c63efcf737e0de2991e4c
Ben Schmaus <ben_schmaus@comcast.com>
2016-08-11 16:20:30 -0400

    initial import of week3 exercise.

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,13 @@ package funsets
 object Main extends App {
   import FunSets._
 
+  println("running");
+}
+
+class Foo {
+
+}
+
+class Bar {
+
 }
---
+++ objsets/src/main/scala/Testing.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by bschma200 on 8/11/16.
+  */
+object Testing extends App {
+
+  println("running")
+
+}
